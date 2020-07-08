7714d1bf59ce7bf3ed44c1fc0ce1f62d3307de98
Chairy-neko <2654460140@qq.com>
2020-07-09 00:24:12 +0800

    HW3

---
+++ app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -0,0 +1,108 @@
+package com.example.chapter3.homework;
+
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.view.animation.AnimationUtils;
+
+public class PlaceholderFragment extends Fragment {
+
+    private static final String TAG = "Fragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
+        Log.d(TAG, "onCreateView() called with: inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]");
+        return inflater.inflate(R.layout.fragment_placeholder, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        getView().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 这里会在 5s 后执行
+                // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                ObjectAnimator Animator3 = ObjectAnimator.ofFloat(getView().findViewById(R.id.animation_view),"alpha",1f,0f);
+                Animator3.setDuration(1000);
+                Animator3.start();
+                ObjectAnimator Animator4 = ObjectAnimator.ofFloat(getView().findViewById(R.id.message),"alpha",0f,1f);
+                Animator4.setDuration(1000);
+                Animator4.start();
+//                public static void animFadeOut(View view) {
+//                    AlphaAnimation animation = new AlphaAnimation(1f, 0);
+//                    animation.setDuration(3000);
+//                    animation.setFillAfter(true);
+//                    if (view != null)
+//                        view.startAnimation(animation);
+//                }
+            }
+        }, 5000);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach() called with: context = [" + context + "]");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach() called");
+    }
+}
